9ad3e5a43938daf6af288bd9cb2332cf4891d3a0
Calvin Wong <ctwong958@berkeley.edu>
2021-03-22 09:38:36 -0700

    Cleanup menu code, small menu tweaks

--- Scn/MainMenu.gd
+++ Scn/MainMenu.gd
@@ -7,35 +7,57 @@ onready var spinner := $Menu/PlayMargin/Play/Center/Spinner
 
 const STANDARD := PoolStringArray(["Off", "Low", "Medium", "High"])
 const BINARY := PoolStringArray(["Off", "On"])
-const ALL_GFX_OPTIONS := PoolStringArray(["shadows", "glow"])
+const ALL_GFX_OPTIONS := PoolStringArray(["shadows", "glow"]) # TODO: update graphics settings
+# TODO: minimal theme, where graphics settings are opened in separate panel
 
 func _ready() -> void:
-	add_options(overall_gfx, PoolStringArray(["Potato", "Low", "Medium", "High"]))
+	# Add options
+	add_options(overall_gfx, PoolStringArray(["Potato", "Low", "Medium", "High", "Custom"]))
 	add_options(shadows, STANDARD)
 	add_options(glow, BINARY)
-	setup_graphics_options($Menu/Gfx/Options)
+	# Setup signals
+	setup_graphics_options_signals($Menu/Gfx/Options, "gfx_changed")
 
-
-func setup_graphics_options(gfx_control: Control) -> void:
+# Connects all OptionButtons under parent GFX_CONTROL to FUNCTION_NAME
+# (Note: OptionButtons must be children of an HBoxContainer under GFX_CONTROL)
+func setup_graphics_options_signals(gfx_control: Control, function_name: String) -> void:
 	for child in gfx_control.get_children():
+		# Specific to this game
 		if child is HBoxContainer:
 			var button : OptionButton = child.get_node("Button")
 			var setting : String = child.name
-			if (button.connect("item_selected", self, "gfx_changed", [setting, button]) != OK):
-				print("COULDN'T CONNECT " + setting)
+			# Connect signal
+			if (button.connect("item_selected", self, function_name, [setting, button]) != OK):
+				print("ERROR: COULDN'T CONNECT " + setting)
+			# Default overall is high
 			if setting == "Overall":
 				button.select(3)
+			# Set other defaults
 			else:
-				button.select(int(min(G.get(setting), button.get_item_count() - 1)))
+				set_setting(setting, G.get(setting))
 
+# If setup_graphics_options_signals connects BUTTON to this, sets SETTING
+# anytime the BUTTON's option is changed.
 func gfx_changed(index: int, setting: String, button: OptionButton) -> void:
+	# Overall setting
 	if setting == "Overall":
-		for option in ALL_GFX_OPTIONS:
-			index = int(min(index, get(option).get_item_count() - 1)) # So that we don't go overbounds
-			G.set(option, index)
-			get(option).select(index)
+		for option in ALL_GFX_OPTIONS: 
+			set_setting(option, index)
+	# Custom setting overall
+	elif index == 4:
+		return
+	# Specific setting
 	else:
-		G.set(setting, int(min(index, button.get_item_count() - 1)))
+		set_setting(setting, index)
+
+# Sets SETTING in G to INDEX (off, low, med, high), corrects overbounds INDEX
+func set_setting(setting: String, index: int) -> void:
+	# Bounds correction
+	index = int(min(index, get(setting).get_item_count() - 1))
+	# Set global variable
+	G.set(setting, index)
+	# Visual update
+	get(setting).select(index)
 
 # Add all strings from OPTIONS as options for BUTTON
 func add_options(button: OptionButton, options: PoolStringArray) -> void:
